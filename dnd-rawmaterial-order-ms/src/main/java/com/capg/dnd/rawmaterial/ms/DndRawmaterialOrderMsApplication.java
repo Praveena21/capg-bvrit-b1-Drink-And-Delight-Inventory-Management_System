31d38d6d78b4a68f3fd8f8b026611b229005f19d
dell <dell@dell-PC>
2020-08-14 17:09:31 +0530

    Added Model Classes

--- dnd-rawmaterial-order-ms/src/main/java/com/capg/dnd/rawmaterial/ms/DndRawmaterialOrderMsApplication.java
+++ dnd-rawmaterial-order-ms/src/main/java/com/capg/dnd/rawmaterial/ms/DndRawmaterialOrderMsApplication.java
@@ -1,4 +1,4 @@
-package com.capg.dnd.rawmaterial.order.ms;
+package com.capg.dnd.rawmaterial.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
