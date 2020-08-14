a9f4b130e495c4d682e00e16ac5c11f2b057ef85
AnithaAnu216 <admin@SUPPORT>
2020-08-14 12:34:38 +0530

    Raw Material Stock updated

--- dnd-rawMaterialStock-ms/src/main/java/com/capg/dnd/rawmaterialstock/ms/service/UpdateRMstockServiceImpl.java
+++ dnd-rawMaterialStock-ms/src/main/java/com/capg/dnd/rawmaterialstock/ms/service/UpdateRMstockServiceImpl.java
@@ -2,38 +2,45 @@ package com.capg.dnd.rawmaterialstock.ms.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.capg.dnd.rawmaterialstock.ms.model.RawMaterialStock;
+import com.capg.dnd.rawmaterialstock.ms.repository.IupdatermstockDAO;
 
+@Service
 public class UpdateRMstockServiceImpl implements IUpdateRMStockService {
-
-	@Override
+	
+	@Autowired
+	IupdatermstockDAO repo;
+	
 	public RawMaterialStock addRawMaterialStock(RawMaterialStock stock) {
-		// TODO Auto-generated method stub
-		return null;
+		return repo.save(stock);
 	}
-
-	@Override
+	
 	public boolean deleteRawMaterialStock(String orderId) {
-		// TODO Auto-generated method stub
-		return false;
+		repo.deleteById(orderId);
+		return (!repo.existsById(orderId));
 	}
-
-	@Override
+	
 	public RawMaterialStock getRawMaterialStockDetails(String orderId) {
-		// TODO Auto-generated method stub
-		return null;
+		return repo.getOne(orderId);
+	}
+	
+	public List<RawMaterialStock> getAllRawMaterialStockDetails(){
+		return repo.findAll();
 	}
 
-	@Override
-	public List<RawMaterialStock> getAllRawMaterialStockDetails() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public RawMaterialStock updateRawMaterialStock(RawMaterialStock newstock) {
 		// TODO Auto-generated method stub
-		return null;
+		RawMaterialStock oldstock=repo.getOne(newstock.getOrderId());
+		oldstock.setManufactuingDate(newstock.getManufactuingDate());
+		oldstock.setExpiryDate(newstock.getExpiryDate());
+		oldstock.setQualityCheck(newstock.getQualityCheck());
+		return repo.save(newstock);
 	}
+
+
+
 
 }
