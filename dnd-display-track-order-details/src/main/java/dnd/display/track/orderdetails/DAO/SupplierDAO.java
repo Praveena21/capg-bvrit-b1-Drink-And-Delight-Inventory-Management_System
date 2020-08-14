a9cf4a7a0e13ff044329f389df3b9cf54fe7c719
mdbahauddin <mdbahauddin1998@gmail.com>
2020-08-14 14:12:18 +0530

    .

--- dnd-display-track-order-details/src/main/java/dnd/display/track/orderdetails/DAO/SupplierDAO.java
+++
@@ -1,9 +0,0 @@
-package dnd.display.track.orderdetails.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dnd.display.track.orderdetails.model.SupplierEntity;
-
-public interface SupplierDAO extends JpaRepository<SupplierEntity,Integer>{
-
-}
