0d7792bf14c0602ffc8d227c440ee140a4aeeefb
Praveena <Praveena@DESKTOP-DHU41K7>
2020-08-18 10:27:40 +0530

    1

--- dnd-distributordetails-ms/src/main/java/com/capg/dnd/distributordetails/exception/DistributorIdNotFoundException.java
+++ dnd-distributordetails-ms/src/main/java/com/capg/dnd/distributordetails/exception/DistributorIdNotFoundException.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "Distributor Not Found", value = HttpStatus.NOT_FOUND)
 public class DistributorIdNotFoundException extends RuntimeException{
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public DistributorIdNotFoundException(String message) 
