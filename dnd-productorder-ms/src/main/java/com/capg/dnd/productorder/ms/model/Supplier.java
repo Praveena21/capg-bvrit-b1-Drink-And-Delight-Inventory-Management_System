4644e1ab54169c33fb6637d6f54c3e9c9945bc6d
mdbahauddin <mdbahauddin1998@gmail.com>
2020-08-20 20:33:27 +0530

    Final commit

---
+++ dnd-productorder-ms/src/main/java/com/capg/dnd/productorder/ms/model/Supplier.java
@@ -0,0 +1,66 @@
+package com.capg.dnd.productorder.ms.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Supplier {
+
+	@Id
+	private Long supplierId;
+	private String name;
+	private String address;
+	private Long phoneNo;
+	
+	public Supplier(Long supplierId, String name, String address, long phoneNo) {
+		super();
+		this.supplierId = supplierId;
+		this.name = name;
+		this.address = address;
+		this.phoneNo= phoneNo;
+	}
+
+	public Supplier() {
+		super();
+	}
+
+	public long getSupplierId() {
+		return supplierId;
+	}
+
+	public void setSupplierId(Long supplierId) {
+		this.supplierId = supplierId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getphoneNo() {
+		return phoneNo;
+	}
+
+	public void setphoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	@Override
+	public String toString() {
+		return "SupplierModel [supplierId=" + supplierId + ", name=" + name + ", address=" + address + ", phoneNo="
+				+ phoneNo + ", getSupplierId()=" + getSupplierId() + ", getName()=" + getName() + ", getAddress()="
+				+ getAddress() + ", getphoneNo()=" + getphoneNo() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+}
---
+++ dnd-productorder-ms/src/test/java/com/capg/dnd/productorder/ms/DndProductorderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.dnd.productorder.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndProductorderMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
